c585c514b45702d0f22fdeb70adc1af19651c811
Vasilevskiy Pasha <vasilevskij00@inbox.ru>
2021-05-06 12:11:33 +0300

    feat: add restaurant adapter

---
+++ app/src/main/java/by/vasilevskiy/simplecaching/features/restaurants/RestaurantAdapter.kt
@@ -0,0 +1,47 @@
+package by.vasilevskiy.simplecaching.features.restaurants
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import by.vasilevskiy.simplecaching.data.Restaurant
+import by.vasilevskiy.simplecaching.databinding.RestaurantItemBinding
+import com.bumptech.glide.Glide
+
+class RestaurantAdapter : ListAdapter<Restaurant, RestaurantAdapter.RestaurantViewHolder>(RestaurantComparator()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RestaurantViewHolder {
+        val binding =
+            RestaurantItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return RestaurantViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: RestaurantViewHolder, position: Int) {
+        val current = getItem(position)
+        if (current != null) {
+            holder.bind(current)
+        }
+    }
+
+    class RestaurantViewHolder(private val binding: RestaurantItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(restaurant: Restaurant) {
+            binding.apply {
+                Glide.with(itemView).load(restaurant.logo).into(imageViewLogo)
+                textViewName.text = restaurant.name
+                textViewType.text = restaurant.type
+                textViewAddress.text = restaurant.address
+            }
+        }
+    }
+
+    class RestaurantComparator : DiffUtil.ItemCallback<Restaurant>() {
+        override fun areItemsTheSame(oldItem: Restaurant, newItem: Restaurant) =
+            oldItem.name == newItem.name
+
+        override fun areContentsTheSame(oldItem: Restaurant, newItem: Restaurant) =
+            oldItem == newItem
+    }
+}
